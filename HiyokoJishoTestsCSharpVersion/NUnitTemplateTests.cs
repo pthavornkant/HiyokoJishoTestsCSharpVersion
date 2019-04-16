dced7e928b2e190bc4098ed97b2e65bbb1495cd7
Patrick <pthavornkant@gmail.com>
2019-04-15 18:51:48 -0700

    Add project files.

---
+++ HiyokoJishoTestsCSharpVersion/NUnitTemplateTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace HiyokoJishoTestsCSharpVersion
+{
+    [TestFixture]
+    public class NUnitTemplateTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.Pass("Sample test passed.");
+        }
+
+        [Test]
+        public void AccessTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.hiyokojisho.com/");
+            String pageTitle = driver.Title;
+            Assert.AreEqual(pageTitle, "Hiyoko Jisho");
+            driver.Quit();
+        }
+    }
+}
---
+++ HiyokoJishoTestsCSharpVersion/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiyokoJishoTestsCSharpVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Hello World");
+            driver.Quit();
+            //driver.Navigate().GoToUrl("https://www.hiyokojisho.org");
+
+        }
+    }
+}
